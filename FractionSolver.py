9ffb192da14870760aa9218c5f67e003a380b22d
Brancucci <brancucci@mail.usf.edu>
2017-03-19 10:35:32 -0400

    everything is working just need to add exceptions

--- FractionSolver.py
+++ FractionSolver.py
@@ -1,23 +1,34 @@
-import re
+import re # used for regular expressions
+from fractions import Fraction # used for fractions
 
-class Fraction():
-    num = 0
-    den = 0
-
-
-# >>> s = "-23/45 + 14/9"
-# >>> re.findall(patt,s)
-# [( "-23","45","+","14","49")
-
-
+# the regular expression used to extract two fractions and an operator
 patt = re.compile(r'(?P<num>-?\d+)/(?P<den>-?\d+)\s*(?P<op>[+\-*/])\s*(?P<num2>-?\d+)/(?P<den2>-?\d+)')
-X = Fraction()
-Y = Fraction()
 
+# loop forever until the user entered a blank string or has an error
 while True:
-    user_string = input("Enter: <fraction> <operater> <fracion> >> ")
-    if user_string:
-        data = re.findall(patt, user_string)
+    user_string = input("Enter: <fraction> <operater> <fracion> >> ") # get the user string
+    if user_string: # if the user entered something ...
+        data = re.findall(patt, user_string) # gives us a 5 tuple
+        num = int(data[0][0])
+        den = int(data[0][1])
+        X = Fraction(num, den) # create a fraction object with the lhs of the equation
+        num1 = int(data[0][3])
+        den1 = int(data[0][4])
+        Y = Fraction(num1, den1) # creates a fraction object with the rhs of the equation
+        op = data[0][2] # extracts the operator from the user
+        # switch statement to apply appropriate operator to calculation
+        if op == '+':
+            R = X + Y
+        elif op == '-':
+            R = X - Y
+        elif op == '*':
+            R = X * Y
+        elif op == '/':
+            R = X / Y
+        else:
+            break
+            # print the results to user
+        print('{} = {}'.format(user_string, R))
 
     else:
         break
